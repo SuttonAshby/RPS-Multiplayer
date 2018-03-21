eb29c243315ce674ac10a6e78778ef58bb0cb9fa
Sutton Ashby <korim_fidelis@yahoo.com>
2018-03-21 10:59:28 -0400

    more game logic (still includes losses scoring)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,11 +8,12 @@ var game = {
 	score: 0, // local player score
 	playerPick: "noPick", //players choice to be pushed to firebase
 	countdownID: undefined, //for clearing interval
-	timer: 15, //timer length
+	timer: 20, //timer length
 	timerRunning: false, //check if timer is running
 	database: undefined, // for reference to firebase
 	players: 0, //number of player need to get value from firebase and update
 	opponentPicks: [], //stores array of opponent choices pulled from firebase
+	matchHappening: false, //store to firebase to check if a match is already running
 	config: {
       apiKey: "AIzaSyCLbEjKvRPiZdO3I6ADlExj_N0ZD60nATk",
       databaseURL: "https://rpsmulti-fd6d4.firebaseio.com",
@@ -27,30 +28,24 @@ var game = {
 			game.timerRunning = false;
 			database.ref("Players").child(game.playerID).update({playerPick: game.playerPick});
 
+			//lights up players button choice
+			var colorPick = '"#' + game.playerPick + 'pic"'
+			$(JSON.parse(colorPick)).addClass("playerPick")
+
+			//gets current opponent picks and then updates score
 			setTimeout(game.getOpponentPicks, 3000)
 			setTimeout(game.updateScore, 4000)
-
-			// game.updateScore()
 		}
 	},
 	countdown: function(){
 		$("#info").text("Match is starting")
-
-		// var matchplayers = database.ref("Players")
-		// matchplayers.on("value", function(snapshot){
-		// 	snapshot.forEach(function(child){
-		// 		if(child !== game.playerID){
-		// 		console.log(child.val().playerPick)
-		// 		game.opponentPicks.push(child.val().playerPick)
-		// 	}
-		// 	})
-
-		// })
-
+		game.matchHappening = true;
+		database.ref().child("matchHappening").set(game.matchHappening);
 		game.timerRunning = true;
 		game.countdownID = setInterval(game.decrementTimer, 1000)
 	},
 	loadButtons: function(){
+		$("#buttons").empty()
 		//BUG on smaller view finder form covers buttons making them unclickable 
 		for(var i = 0; i < 5; i++){
 			var element = $("<div>")
@@ -59,7 +54,8 @@ var game = {
 					  .attr("id", id[0])
 			var btnImg = $("<img>")
 			$(btnImg).attr("src", "assets/images/" + game.displayImgs[i])
-					  .addClass("buttonImg")
+					 .attr("id", id[0] + "pic")
+					 .addClass("buttonImg")
 			$(element).append(btnImg)
 			$("#buttons").append(element)
 		}
@@ -82,16 +78,14 @@ var game = {
 		//gets number of players already in game
 		var playerCount = database.ref("playerCount")
 		playerCount.once("value", function(snapshot){
-			console.log(snapshot.val())
 			game.players = snapshot.val()
 		});
 
+		//initial snapshot to load any players already in game
+		//BUG seems to run twice
 		var players = database.ref("Players")
 		players.once("value", function(snapshot){
 			snapshot.forEach(function(child){
-				console.log(child.val().name)
-				console.log(child.val().wins)
-				console.log(child.val().losses)
 				var element = $("<div>")
 				var player = child.val().name
 				var wins = child.val().wins
@@ -100,10 +94,8 @@ var game = {
 			})
 		})
 
-
-
 		//sets on click events for buttons
-		$(".button").on("click", game.playerChoice)
+		$("#buttons").on("click", ".button", game.playerChoice)
 		$("#submit").on("click", game.playerName)
 
 		// sets connection information and onDisconnect functionality
@@ -116,29 +108,21 @@ var game = {
 				con.onDisconnect().remove()
 			}
 		})
-		connectionsRef.on("value", function(snap){
-			database.ref(game.playerID).remove()
-			if(snap.val() === false){
-				game.players--;
-				database.ref("playerCount").set(game.players)
-			}
-			
-		})
-
 
 		// updates player count locally when firebase updates player count
 		database.ref("Players").on("child_added", game.updatePlayerList)
-		// database.ref("Players").on("value", game.getOpponentPicks)
-		// 	console.log()
-		// })
 
-		//Initiate match once there is more than one player
-		var playerCount = database.ref("playerCount")
+
+		//Initiate match once there is more than one player and there isn't already a match in progress
+		var playerCount = database.ref()
 			playerCount.on("value", function(snap){
-				if(snap.val() > 1){
+				if(snap.child("playerCount").val() > 1 && snap.child("matchHappening").val() !== true){
 					playerCount.off()
 					game.countdown()
 				}
+				// if(snap.child("matchHappening") === true){
+				// 	playerCount.off()
+				// }
 				
 			});
 
@@ -146,20 +130,24 @@ var game = {
 	getOpponentPicks: function(){
 
 		//WORKS BUT UNSTABLE
+		//including own score doesn't matter in positive only scoring,
+		//maybe remove check system.
 		var matchplayers = database.ref("Players")
 		matchplayers.once("value", function(snapshot){
 			snapshot.forEach(function(child){
-				// console.log("Snapshot val is:" + snapshot.val().name)
-				// console.log("child val is:" +child.val().name)
 				if(child.val().name !== game.playerID){
-				// console.log(child.val().playerPick)
-				// console.log(child.val().name)
 				game.opponentPicks.push(child.val().playerPick)
 			}
 			})
 
 		})
-		console.log(game.opponentPicks)
+		for(var i = 0; i < game.opponentPicks.length; i++){
+			var opponentColor = '"#' + game.opponentPicks[i] + 'pic"';
+			$(JSON.parse(opponentColor)).addClass("opponentPick")
+			var choice = '".' + game.opponentPicks[i] + 'Img"';
+			$(JSON.parse(choice)).css("visibility", "visible")
+
+		}
 	},
 	allInvis: function(){
 		$(".displayImg").css("visibility", "hidden");
@@ -172,34 +160,111 @@ var game = {
 		var choice = '".' + $(this).attr("id") + 'Img"';
 		$(JSON.parse(choice)).css("visibility", "visible")
 		game.playerPick = $(this).attr("id")
+
 		
 	},
 	updateScore: function(){
-		// var wins = database.ref(game.playerID)
-		// wins.on("value", function(snapshot){
-			// console.log(snapshot.val())
+		//NEED TO ADD GETTING A POINT IF THE OPPONENT DOESNT PICK
+		//calculates wins for local player and relevent  visuals
 		for(var i = 0; i < game.opponentPicks.length; i++){
-			if(game.playerPick ===  "Rock" && (game.opponentPicks[i] === "Lizard" || game.opponentPicks[i] === "Scissors")){
+			if(game.playerPick === "Rock" && (game.opponentPicks[i] === "Lizard" || game.opponentPicks[i] === "Scissors")){
 				game.score++;
-				database.ref(game.playerID).update({wins: game.score})
-			} else if(game.playerPick ===  "Paper" && (game.opponentPicks[i] === "Spock" || game.opponentPicks[i] === "Rock")){
+				database.ref("Players").child(game.playerID).update({wins: game.score})
+				if(game.opponentPicks[i] === "Lizard"){
+					$(".RbLImg").css("visibility", "visible")
+				} else {
+					$(".RbSImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Paper" && (game.opponentPicks[i] === "Spock" || game.opponentPicks[i] === "Rock")){
 				game.score++;
-				database.ref(game.playerID).update({wins: game.score})
-			} else if(game.playerPick ===  "Scissors" && (game.opponentPicks[i] === "Lizard" || game.opponentPicks[i] === "Paper")){
+				database.ref("Players").child(game.playerID).update({wins: game.score})
+				if(game.opponentPicks[i] === "Spock"){
+					$(".PbVImg").css("visibility", "visible")
+				} else {
+					$(".PbRImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Scissors" && (game.opponentPicks[i] === "Lizard" || game.opponentPicks[i] === "Paper")){
 				game.score++;
-				database.ref(game.playerID).update({wins: game.score})
-			} else if(game.playerPick ===  "Lizard" && (game.opponentPicks[i] === "Spock" || game.opponentPicks[i] === "Paper")){
+				database.ref("Players").child(game.playerID).update({wins: game.score})
+				if(game.opponentPicks[i] === "Lizard"){
+					$(".SbLImg").css("visibility", "visible")
+				} else {
+					$(".SbPImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Lizard" && (game.opponentPicks[i] === "Spock" || game.opponentPicks[i] === "Paper")){
 				game.score++;
-				database.ref(game.playerID).update({wins: game.score})
-			} else if(game.playerPick ===  "Spock" && (game.opponentPicks[i] === "Scissors" || game.opponentPicks[i] === "Rock")){
+				database.ref("Players").child(game.playerID).update({wins: game.score})
+				if(game.opponentPicks[i] === "Spock"){
+					$(".LbVImg").css("visibility", "visible")
+				} else {
+					$(".LbPImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Spock" && (game.opponentPicks[i] === "Scissors" || game.opponentPicks[i] === "Rock")){
 				game.score++;
-				database.ref(game.playerID).update({wins: game.score})
+				database.ref("Players").child(game.playerID).update({wins: game.score})
+				if(game.opponentPicks[i] === "Scissors"){
+					$(".VbSImg").css("visibility", "visible")
+				} else {
+					$(".VbRImg").css("visibility", "visible")
+				}
 			}
 		}
-		console.log("SCORE is:" + game.score)
+		//loads up visuals for oppoent wins
+		for(var i = 0; i < game.opponentPicks.length; i++){
+			if(game.playerPick === "Rock" && (game.opponentPicks[i] === "Paper" || game.opponentPicks[i] === "Spock")){
+				if(game.opponentPicks[i] === "Paper"){
+					$(".PbRImg").css("visibility", "visible")
+				} else {
+					$(".VbRImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Paper" && (game.opponentPicks[i] === "Scissors" || game.opponentPicks[i] === "Lizard")){
+				if(game.opponentPicks[i] === "Scissors"){
+					$(".SbPImg").css("visibility", "visible")
+				} else {
+					$(".LbPImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Scissors" && (game.opponentPicks[i] === "Spock" || game.opponentPicks[i] === "Rock")){
+				if(game.opponentPicks[i] === "Spock"){
+					$(".VbSImg").css("visibility", "visible")
+				} else {
+					$(".RbSImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Lizard" && (game.opponentPicks[i] === "Rock" || game.opponentPicks[i] === "Scissors")){
+				if(game.opponentPicks[i] === "Rock"){
+					$(".RbLImg").css("visibility", "visible")
+				} else {
+					$(".SbLImg").css("visibility", "visible")
+				}
+			} else if(game.playerPick === "Spock" && (game.opponentPicks[i] === "Paper" || game.opponentPicks[i] === "Lizard")){
+				if(game.opponentPicks[i] === "Paper"){
+					$(".PbVImg").css("visibility", "visible")
+				} else {
+					$(".LbVImg").css("visibility", "visible")
+				}
+			}
+		}
 
-		// })
+		setTimeout(game.displayScore, 3000)
+
 		
+	},
+	displayScore: function(){
+		var scores = database.ref("Players")
+		scores.once("value", function(snapshot){
+			$("tbody").empty()
+			snapshot.forEach(function(child){
+				var element = $("<div>")
+				var player = child.val().name
+				var wins = child.val().wins
+				var losses = child.val().losses
+				$("tbody").append("<tr><td>" + player + "</td><td>" + wins + "</td><td>" + losses + "<td></tr>")	
+			})
+		})
+		database.ref("matchHappening").set(false)
+
+		setTimeout(game.rematch, 2000)
+
+
 	},
 	playerName: function(){
 		if(game.playerID === undefined){
@@ -213,17 +278,32 @@ var game = {
 			game.players++;
 			database.ref("playerCount").set(game.players)	
 		}
-		// game.countdown()	
+		//sets on disconnect info for player data and player count
+		database.ref("Players").child(game.playerID).onDisconnect().remove()
+		var playerNum = database.ref("playerCount")
+		playerNum.on("value", function(snapshot){
+			game.players = snapshot.val()
+			database.ref("playerCount").onDisconnect().set(--game.players)
+		})
+		
 	},
 	updatePlayerList: function(snapshot){
-		//BUG sometimes posts undefined data (probably on removal of child OR when countdown finishes)
-		var element = $("<div>")
+		//BUG sometimes posts undefined data (probably on removal of child OR when countdown finishes) **possibly resolved now**
 		var player = snapshot.val().name
 		var wins = snapshot.val().wins
 		var losses = snapshot.val().losses
 		$("tbody").append("<tr><td>" + player + "</td><td>" + wins + "</td><td>" + losses + "<td></tr>")
 
 	},
+	rematch: function(){
+		game.allVis();
+		game.loadButtons();
+		game.timer = 20;
+		game.opponentPick = [];
+		game.playerPick = "noPick"
+		game.countdown();
+
+	}
 
 }
 
